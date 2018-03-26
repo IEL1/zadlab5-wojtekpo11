2c8de3c29d34e30ae554e54d9e3180077fcce101
student <student@pc-108-21>
2018-03-26 18:14:34 +0200

    lab

---
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/NewMain1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl5;
+
+/**
+ *
+ * @author student
+ */
+public class NewMain1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        książka k1 = new książka("Kordian", "Juliusz Słowacki", 200, 1833, 5.99);
+        książka k2 = new książka("Opowieści z Narnii: Lew, Czarownica i stara szafa", "Andrew Adamson", 183, 1949, 29.99);
+        książka k3 = new książka("W pustyni i w puszczy", "Henryk Sienkiewicz", 327, 1911, 11.99);
+    }
+
+}
