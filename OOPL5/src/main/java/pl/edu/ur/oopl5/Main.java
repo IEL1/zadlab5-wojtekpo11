d20f2dd6c5cadf5e9b9d86254a1c51cd7dffad42
Wojciech <Wojciech@DESKTOP-LHA1MAK>
2018-04-15 11:14:34 +0200

    Main

--- OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
@@ -1,9 +1,44 @@
 package pl.edu.ur.oopl5;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // TODO zad 3, 5, 6
+    private static void zadanie3() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Podaj rozmiar stosu: ");
+        int rozmiarStosu = in.nextInt();
+        Stack Stak = new Stack(rozmiarStosu);
+        Stak.menu();
     }
-    
+
+    private static void zadanie5() {
+        Firma company1 = new Firma();
+        company1.menu();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int menu = 1;
+
+        while (menu != 0) {
+            System.out.println("3. Zadanie 3");
+            System.out.println("5. Zadanie 5");
+            System.out.println("0. Wyjscie");
+            menu = in.nextInt();
+
+            switch (menu) {
+                case 3:
+                    zadanie3();
+                    break;
+                case 5:
+                    zadanie5();
+                    break;
+                case 0:
+                    break;
+            }
+        }
+    }
 }
